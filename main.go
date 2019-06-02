379dfcd4ada6c745330c54ee5034217a1138a0f2
reunion <takahiro.hoshi0130@gmail.com>
2019-06-02 15:45:49 +0900

    add cli

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/urfave/cli"
+	"golang.org/x/xerrors"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+)
+
+type StatusCode struct {
+	Code        int    `json:"code"`
+	Explanation string `json:"message"`
+}
+
+var statuses []StatusCode
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "httpstatus"
+	app.Usage = "show http status code"
+	app.Version = "1.0.0"
+
+	app.Before = func(c *cli.Context) error {
+		bytes, err := ioutil.ReadFile("status.json")
+		if err != nil {
+			return err
+		}
+		if err = json.Unmarshal(bytes, &statuses); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	app.Commands = []cli.Command {
+		{
+			Name: "all",
+			Aliases: []string{"l"},
+			Usage: "print all of http status code",
+			Action: func(c *cli.Context) error {
+				return all()
+			},
+		},
+		{
+			Name: "find",
+			Aliases: []string{"s"},
+			Usage: "find by code",
+			Action: func(c *cli.Context) error {
+				return find(c.Args().First())
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func find(code string) (err error) {
+	c, _ := strconv.Atoi(code)
+	for _, s := range statuses {
+		if c == s.Code {
+			fmt.Printf("%d : %s\n", s.Code, s.Explanation)
+			return
+		}
+	}
+	return xerrors.New("input code not found")
+}
+
+func all() (err error) {
+	for _, s := range statuses {
+		fmt.Printf("%d : %s\n", s.Code, s.Explanation)
+	}
+	return
+}
